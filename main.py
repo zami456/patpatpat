ac48c0e910667008b5eb57ca2c42f1f9b479511a
zami456 <abdullah.al.zami@g.bracu.ac.bd>
2024-12-26 23:17:17 +0600

    add searching filtering and pet adding feature

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
--- website/views.py
+++ website/views.py
@@ -1,11 +1,58 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, jsonify
 from flask_login import login_required, current_user
 from . import mysql
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        city = request.form.get('city')
+        area = request.form.get('area')
+        pet_type = request.form.get('pet_type')
+        age = request.form.get('age')
+        vaccinated = request.form.get('vaccinated')
+        sex = request.form.get('sex')
+        neutered = request.form.get('neutered')
+        search = request.form.get('search')
+
+        query = 'SELECT * FROM pets WHERE 1=1'
+        params = []
+
+        if city:
+            query += ' AND city = %s'
+            params.append(city)
+        if area:
+            query += ' AND area = %s'
+            params.append(area)
+        if pet_type:
+            query += ' AND pet_type = %s'
+            params.append(pet_type)
+        if age:
+            query += ' AND age = %s'
+            params.append(age)
+        if vaccinated:
+            query += ' AND vaccinated = %s'
+            params.append(vaccinated)
+        if sex:
+            query += ' AND sex = %s'
+            params.append(sex)
+        if neutered:
+            query += ' AND neutered = %s'
+            params.append(neutered)
+        if search:
+            query += ' AND (name LIKE %s OR breed LIKE %s OR description LIKE %s)'
+            search_param = f"%{search}%"
+            params.extend([search_param, search_param, search_param])
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(query, params)
+        pets = cursor.fetchall()
+        cursor.close()
+
+        # Return JSON response for AJAX request
+        return jsonify({'pets': pets})
+
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT * FROM pets')
     pets = cursor.fetchall()
